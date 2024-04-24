6dc6431e612c4beb84ca46882148f416fda8c91e
KFrodente <kianfrodente@gmail.com>
2024-04-23 23:06:32 -0600

    Add project files.

---
+++ game/src/main.c
@@ -0,0 +1,17 @@
+#include "raylib.h"
+
+int main(void)
+{
+	InitWindow(800, 450, "raylib [core] example - basic window");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Congrats!", 190, 200, 20, MAROON);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
